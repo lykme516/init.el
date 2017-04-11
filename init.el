02f375e077ca654ac2d42b51293253696d9e68ce
Stephen Jung <sjung516@gmail.com>
2017-04-11 14:43:23 -0400

    Converted to elpy.

--- init.el
+++ init.el
@@ -122,6 +122,9 @@
 ;; Impatient Mode
 ;; wikemacs.org/wiki/Markdown#Live_preview_as_you_type
 
+(require 'impatient-mode)
+(add-hook 'html-mode-hook #'impatient-mode)
+
 (defun markdown-html (buffer)
   (princ (with-current-buffer buffer
 	   (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s </xmp><script 
@@ -132,23 +135,29 @@ src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-subs
 ;; Python Mode
 
 ;; https://github.com/jorgenschaefer/elpy
-(require 'package)
-(add-to-list 'package-archives
-	     '("elpy" . "https://jorgenschaefer.github.io/packages/"))
+(elpy-enable)
 
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'fly-check-mode))
 
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+(elpy-use-ipython)
+(setq python-shell-interpreter "ipython" python-shell-interpreter-args "--simple-prompt --pprint")
 
 ; try to automagically figure out indentation
-(setq py-smart-indentation t)
+;(setq py-smart-indentation t)
 ;; set tabs to spaces and set tab width to 4
-(add-hook 'python-mode-hook
+(add-hook 'elpy-mode-hook
 	  (function (lambda ()
 		      (setq indent-tabs-mode nil
 			    tab-width 4))))
 
 ;; Web Mode
 (require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'".web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (setq web-mode-engines-alist '(("django" . "\\.html\\'")))
 
@@ -156,7 +165,7 @@ src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-subs
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 
-(setq web-mode-enable-auto-pairing t)
+;(setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-auto-expanding t)
 (setq web-mode-enable-css-colorization t)
 
@@ -166,7 +175,11 @@ src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-subs
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (hickey)))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
     ("96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "e0d42a58c84161a0744ceab595370cbe290949968ab62273aed6212df0ea94b4" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" default)))
